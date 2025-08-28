41caf21762c3f7997759e2442e0ee3e00a1cb65d
unknown <andrei.fbds@gmail.com>
2025-08-28 20:10:22 -0300

    Frontend initial configuration + register/login page with React

---
+++ frontend/src/components/User.tsx
@@ -0,0 +1,108 @@
+import { useForm } from "react-hook-form";
+import "./App.css";
+import backgroundImage from "../assets/pexels-fauxels-3183197.jpg";
+
+type FormData = {
+  username: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+};
+const User = () => {
+  const {
+    register,
+    handleSubmit,
+    setError,
+    formState: { errors },
+  } = useForm<FormData>();
+
+  return (
+    <div className="flex items-center justify-center h-screen bg-black overflow-hidden">
+      <div className="relative w-full h-full overflow-hidden">
+        <img
+          src={backgroundImage}
+          className="absolute inset-0 w-full h-full object-cover blur-xs z-0 scale-140 opacity-80"
+          alt="Foto de fauxels: https://www.pexels.com/pt-br/foto/foto-de-pessoas-fazendo-apertos-de-mao-3183197/"
+        />
+        <div className="relative z-10 flex items-center justify-center h-full">
+          <p
+            className="text-center text-white scale-325 font-medium text-shadow-lg"
+            style={{
+              textShadow: "2px 2px 4px rgba(1,1,1,1)",
+            }}
+          >
+            Organize{" "}
+            <span
+              className="text-transparent font-storyscript"
+              style={{
+                WebkitTextStroke: "0.2px white",
+              }}
+            >
+              your flow
+            </span>
+            , achieve{" "}
+            <span
+              className="text-transparent font-storyscript"
+              style={{
+                WebkitTextStroke: "0.2px white",
+              }}
+            >
+              your goals
+            </span>
+            .
+            <br />
+          </p>
+        </div>
+      </div>
+      <div className="flex shadow-4xl w-[30vw] h-screen z-1 bg-[white] justify-center items-center shadow-lg">
+        <form>
+          <div className="flex flex-col font-bold text-[#164636]">
+            <h1 className="mb-10 mt-3 mx-auto text-3xl">Register/Login</h1>
+            <label htmlFor="username">Username</label>
+            <input
+              type="text"
+              name="username"
+              placeholder="Enter your username"
+              className="p-2 rounded-lg shadow-2xl border-3 mt-2 mb-5"
+            />
+            <label htmlFor="e-mail">E-mail</label>
+            <input
+              type="text"
+              name="e-mail"
+              placeholder="Enter your e-mail"
+              className="p-2 rounded-lg shadow-2xl border-3 mt-2 mb-5"
+            />
+            <label htmlFor="password">Password</label>
+            <input
+              type="text"
+              name="Password"
+              placeholder="Enter your password"
+              className="p-2 rounded-lg shadow-2xl border-3 mt-2 mb-5"
+            />
+            <label htmlFor="confirmPassword">Confirm password</label>
+            <input
+              type="text"
+              name="confirmPassword"
+              placeholder="Confirm your password"
+              className="p-2 rounded-lg shadow-2xl border-3 mt-2 mb-5"
+            />
+            <button className="pt-2 pb-2 pl-4 pr-4 bg-blue-700 text-white mb-2 hover:bg-blue-950 cursor-pointer">
+              Register
+            </button>
+            <button className="pt-2 pb-2 pl-4 pr-4 bg-green-700 text-white mb-5 hover:bg-green-950 cursor-pointer">
+              Login
+            </button>
+            <a
+              className="font-light text-sm hover:text-[#0e3629] hover:underline"
+              href=""
+            >
+              Forgot my password
+            </a>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default User;
