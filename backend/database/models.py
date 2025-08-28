6a41afb398ee71a9ba5143d85392ee4498b95500
unknown <andrei.fbds@gmail.com>
2025-08-28 12:26:50 -0300

    User Model Created and first FastAPI configurations

---
+++ backend/database/models.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+from uuid import UUID, uuid4
+from pydantic import EmailStr
+from sqlmodel import Field, SQLModel
+from sqlalchemy import Column
+from sqlalchemy.dialects import postgresql
+
+class User(SQLModel, table=True):
+    id: UUID = Field(sa_column=Column(postgresql.UUID, default=uuid4(), primary_key=True, index=True))
+    username: str
+    email: EmailStr
+    password_hashed: str
+    created_at: datetime = Field(default=datetime.now())
